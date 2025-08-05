65daf70baa7a24ed3ec5fb6e98ce989e87a94928
Ashwin <ashvinkumar8891@gmail.com>
2025-08-05 15:38:31 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./App.css";
+import ModalForm from "./components/ModalForm";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "600px",
+    height: "auto",
+    // backgroundColor: "#f0f0f0",
+    borderRadius: "10px",
+    // padding: "20px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",    
+  },
+};
+
+// Set the app element for accessibility
+Modal.setAppElement("#root");
+
+export default function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  // Function to open the modal
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+  return (
+    <div>
+      <h1>UserDetails Modal</h1>
+      <button onClick={openModal}>Open Form</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={customStyles}
+        contentLabel="User Details Form"
+      >
+        <ModalForm />
+      </Modal>
+      {/* Render the ModalForm component */}
+    </div>
+  );
+}
---
+++ src/components/ModalForm.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "./ModalForm.css";
+
+export default function ModalForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [dob, setDob] = useState("");
+
+  const today = new Date().toISOString().split("T")[0];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (phone.length < 10) {
+      alert("Invalid phone number. Please enter 10-digit phone number.");
+    }
+    if (dob == today || dob > today) {
+      alert("Invalid date of birth. Date of birth cannot be in the future.");
+    }
+    setName("");
+    setEmail("");
+    setPhone("");
+    setDob("");
+  };
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <h2>Fill Details</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <h4>Username:</h4>
+          <input
+            type="text"
+            id="username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <h4>Email Address:</h4>
+          <input
+            type="email"
+            name=""
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <h4>Phone Number:</h4>
+          <input
+            type="number"
+            name=""
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            required
+          />
+          <h4>Date of Birth:</h4>
+          <input
+            type="date"
+            name=""
+            id="dob"
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
+            required
+            max={today} // Prevent future dates
+          />
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
