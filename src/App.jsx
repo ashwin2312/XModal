e7a52db2416f2d5a65c2ea9b4bb5f1eb8889fcd5
Ashwin <ashvinkumar8891@gmail.com>
2025-08-20 13:18:50 +0530

    click event

--- src/App.jsx
+++ src/App.jsx
@@ -41,11 +41,11 @@ export default function App() {
     };
 
     if (modalIsOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("click", handleClickOutside);
     }
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, [modalIsOpen]);
 
